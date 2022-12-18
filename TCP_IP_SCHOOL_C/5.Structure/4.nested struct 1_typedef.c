ca2f04b726fe124f3b5002cfcaf884f14c09871a
황병국 <hbk6939@gmail.com>
2022-12-18 14:49:25 +0000

    study c

---
+++ TCP_IP_SCHOOL_C/5.Structure/4.nested struct 1_typedef.c
@@ -0,0 +1,34 @@
+// 중첩된 구조체
+// C언어에서는 구조체를 정의할 때 멤버 변수로 또 다른 구조체를 포함할 수 있습니다.
+
+#include <stdio.h>
+
+typedef struct
+{
+	char first[30];
+	char last[30];
+} Name;
+
+typedef struct
+{
+	Name friend_name;
+	char address[30];
+	char job[30];
+} Friends;
+
+int main(void)
+{
+	Friends hong = 
+	{
+		{ "길동", "홍" },
+		"서울시 강남구 역삼동",
+		"학생"
+	};
+	
+	printf("%s\n\n", hong.address);
+	printf("%s%s에게,\n", hong.friend_name.last, hong.friend_name.first);
+	printf("그동안 잘 지냈니? 아직 %s이지?\n", hong.job); 
+	puts("공부 잘 하고, 다음에 꼭 한번 보자.\n잘 지내^^");
+	return 0;
+}
+
